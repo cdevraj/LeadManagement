781aacd0d21fd5d334d2301fb14a1ba2906b547d
sachin <skrathore.ror@gmail.com>
2017-09-12 17:54:53 +0530

    add new template

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
-  resources :users
+  resources :users 
+  get 'lead_generators', to: 'users#lead_generators'	
   resources :leads
+  resources :email_templates
+  get 'send_email', to: 'email_templates#send_email'	
+
+  # Admin routes  
+	namespace :admin do
+  	  get 'list_users', to: "users#list_users"
+  	  post 'assign_manager', to: "users#assign_manager"
+	end
+
 end
