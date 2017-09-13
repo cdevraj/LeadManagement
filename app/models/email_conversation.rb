37a1f7cae2aa8f6ece495d6f58378d5fdd3ff070
sachin <skrathore.ror@gmail.com>
2017-09-13 17:46:30 +0530

    fixed crash

--- app/models/email_conversation.rb
+++ app/models/email_conversation.rb
@@ -8,11 +8,11 @@ class EmailConversation < ApplicationRecord
       date = "#{params[:send_date]["(1i)"]}/#{params[:send_date]["(2i)"]}/#{params[:send_date]["(3i)"]}"
       send_date = Date.parse(date)
       unless send_date > Date.today
-        UserMailer.send_email(params).deliver   
+        UserMailer.send_email(params).deliver rescue nil  
         is_sent = true     
       end  
     else
-      UserMailer.send_email(params).deliver   
+      UserMailer.send_email(params).deliver rescue nil
       is_sent = true     
     end  	
   	EmailConversation.create(sender_id: user.id, receiver_id: params[:receiver_id], receiver_email: params[:receiver_email], subject: params[:subject], content: params[:content], smtp: params[:smtp], is_sent: is_sent, sent_date: send_date, created_at: Time.now)
